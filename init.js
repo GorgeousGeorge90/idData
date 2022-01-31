efe4b3117216ea2edb7f019a40038ed673fcbd7f
Egor <Gambit47@yandex.ru>
2022-01-31 07:21:46 +0300

    first push

---
+++ init.js
@@ -0,0 +1,8 @@
+
+document.querySelector('#btnGenerate').addEventListener('click', ()=> {
+    const initPerson = personGenerator.getPerson();
+    document.querySelector('#genderOutput').innerText = initPerson.gender;
+    document.querySelector('#firstNameOutput').innerText = initPerson.firstName;
+    document.querySelector('#surnameOutput').innerText = initPerson.surname;
+});
+
